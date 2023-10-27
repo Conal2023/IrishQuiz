77d76cc62bcb0985ecd0b11fd81402f6b2e6ecad
Conal2023 <>
2023-10-27 21:04:44 +0000

    Add 3rd question and answer

--- run.py
+++ run.py
@@ -17,12 +17,25 @@ else:
 # QUESTION 2
 answer2 = input("What provence of Ireland is Dublin in? \na. Ulster \
  \nb. Leinster \nc. Munster  \nAnswer: ")
-if answer2 == "b" or answer1 == "Leinster":
+if answer2 == "b" or answer2 == "Leinster":
     score += 1
     print("Correct!")
     print("Score: ", score)
     print("\n")
 else:
     print("Incorrect! The answer is Leinster")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 3
+answer3 = input("What is the only county that doesn't touch another \
+county which touches the sea? \na. Laois \nb. Offaly \nc. Kildare \nAnswer: ")
+if answer3 == "a" or answer3 == "Laois":
+    score += 1
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Laois")
     print("Score: ", score)
     print("\n")
