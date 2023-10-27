0d1e7cbaff4d0a8c865358e144e5e8654abee797
Conal2023 <>
2023-10-27 20:59:04 +0000

    Add welcome note and first 2 question. Fixed all errors before continuing

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Hello and welcome to the Irish Quiz")
+score = 0
+
+# QUESTION 1
+answer1 = input("What is the Capital of Ireland? \na. Limerick \nb. Cork \
+\nc. Dublin \nAnswer: ")
+if answer1 == "c" or answer1 == "Dublin":
+    score += 1
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Dublin")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 2
+answer2 = input("What provence of Ireland is Dublin in? \na. Ulster \
+ \nb. Leinster \nc. Munster  \nAnswer: ")
+if answer2 == "b" or answer1 == "Leinster":
+    score += 1
+    print("Correct!")
+    print("Score: ", score)
+    print("\n")
+else:
+    print("Incorrect! The answer is Leinster")
+    print("Score: ", score)
+    print("\n")
