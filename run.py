91226b1cc9700b22b12b48a731b2b6b664bea854
Conal2023 <>
2023-10-28 20:28:26 +0000

    Change Q3 and tidied up Q1/2

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ print("Hello and welcome to the Irish Quiz")
 score = 0
 
 # QUESTION 1
-answer1 = input("What is the Capital of Ireland? \na. Limerick \nb. Cork \
-\nc. Dublin \nAnswer: ")
+answer1 = input("What is the Capital of Ireland? \
+    \na. Limerick \nb. Cork \nc. Dublin \nAnswer: ")
 if answer1 == "c" or answer1 == "Dublin":
     score += 1
     print("Correct!")
@@ -15,8 +15,8 @@ else:
     print("\n")
 
 # QUESTION 2
-answer2 = input("What provence of Ireland is Dublin in? \na. Ulster \
- \nb. Leinster \nc. Munster  \nAnswer: ")
+answer2 = input("What provence of Ireland is Dublin in? \
+    \na. Ulster \nb. Leinster \nc. Munster  \nAnswer: ")
 if answer2 == "b" or answer2 == "Leinster":
     score += 1
     print("Correct!")
@@ -28,20 +28,20 @@ else:
     print("\n")
 
 # QUESTION 3
-answer3 = input("What is the only county that doesn't touch another \
-county which touches the sea? \na. Laois \nb. Offaly \nc. Kildare \nAnswer: ")
-if answer3 == "a" or answer3 == "Laois":
+answer3 = input("What is largest county in Ireland? \
+    \na. Cork \nb. Limerick \nc. Kerry \nAnswer: ")
+if answer3 == "a" or answer3 == "Cork":
     score += 1
     print("Correct!")
     print("Score: ", score)
     print("\n")
 else:
-    print("Incorrect! The answer is Laois")
+    print("Incorrect! The answer is Cork")
     print("Score: ", score)
     print("\n")
 
 # QUESTION 4
-answer4 = input("What is the most northerly county in Ireland \
+answer4 = input("What is the most northerly county in Ireland? \
  \na. Derry \nb. Donegal \nc. Antrim \nAnswer: ")
 if answer4 == "b" or answer4 == "Donegal":
     score += 1
